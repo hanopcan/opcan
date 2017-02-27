f97d380ed0ed0295f423bbf1033677ba9a9bdad0
Hannah Smith <hannah@opcan.co.uk>
2017-02-27 15:24:47 +0000

    Child theme of twentysixteen created

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+//don't show admin bar in front-end
+show_admin_bar( false );
+
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+?>
