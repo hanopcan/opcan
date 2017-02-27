cf1464f1cb68cd372fa545aa3a42048e85f326a6
Hannah Smith <hannah@opcan.co.uk>
2017-02-27 16:43:14 +0000

    Basic layout for homepage

--- functions.php
+++ functions.php
@@ -8,4 +8,11 @@ function my_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
+
+//allow us to use icons in the post/page content
+function load_dashicons_front_end() {
+    wp_enqueue_style( 'dashicons' );
+}
+add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end');
+
 ?>
