fce80feaf1ce3a544c86be9af81f4cc1a00085ac
Hannah Smith <hannah@opcan.co.uk>
2017-04-13 14:07:58 +0100

    Condensed functions.php so it's a bit tidier

--- functions.php
+++ functions.php
@@ -5,14 +5,16 @@ show_admin_bar( false );
 
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    //enqueue the debugging script
+    wp_enqueue_script('myscript', get_stylesheet_directory_uri().'/js/toggledisplay.js', array('jquery'), '1.0', 'screen, projection');    
+    
+    //allow us to use icons in the post/page content
+    wp_enqueue_style( 'dashicons' );
+
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
-//allow us to use icons in the post/page content
-function load_dashicons_front_end() {
-    wp_enqueue_style( 'dashicons' );
-}
-add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end');
 
 ?>
