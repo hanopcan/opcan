892c6d1801f1fff8008424a8fb33f6a183a6d8d5
Hannah Smith <hannah@highrise.digital>
2018-06-30 19:08:53 +0100

    Button styling in footer

--- partials/footers/footer-contact-cta.php
+++ partials/footers/footer-contact-cta.php
@@ -2,6 +2,6 @@
     <div class="footer-contact-cta-inner">
         <p class="footer-title"> Interested? Great! Whatever the size of your problem, there's nothing to lose. Drop me a line and we can have a no obligation chat about how I could help.</p>
         
-        <a href="/contact"><button class="btn">Contact Hannah</button></a>
+        <a href="/contact"><button class="btn">Contact Hannah <span class="icon fas fa-arrow-right"></span></button></a>
     </div>    
 </div>    
