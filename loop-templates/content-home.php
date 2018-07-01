8d3e4d6c0158793c893088c56d764827a765c16b
Hannah Smith <hannah@highrise.digital>
2018-07-01 15:26:11 +0100

    Add slider to homepage

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -6,6 +6,9 @@
  */
 
 ?>
+
+<?php echo do_shortcode( '[rev_slider alias="home"]' ); ?>
+
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
