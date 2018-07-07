54036d664b3cc28752b5f76814224830c566989c
Hannah Smith <hannah@highrise.digital>
2018-07-07 15:12:07 +0100

    More padding and margin styling for learn page

--- partials/learn/bristol-wordpress.php
+++ partials/learn/bristol-wordpress.php
@@ -1,13 +1,10 @@
-<section>	
+<section class="bristol-wordpress">	
 	<h2>I organise the Bristol WordPress group</h2>
 
 	<div class="row">
 
 		<div class="col-12 col-md-6">
-			<img src="http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group.jpg">
-		</div>	
 
-		<div class="col-12 col-md-6">
 			<p>We are a friendly, approachable and free community for everyone who works with WordPress.</p>
 
 			<a href="https://wpbristol.co.uk" target="_blank" rel="noopener">
@@ -15,6 +12,12 @@
 			</a>
 
 		</div>	
+
+		<div class="col-12 col-md-6">
+
+			<img class="alignnone wp-image-475 size-large" src="http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group-1024x768.jpg" alt="" srcset="http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group-1024x768.jpg 1024w, http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group-300x225.jpg 300w, http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group-768x576.jpg 768w, http://localhost:8888/opcan/wp-content/uploads/2018/05/Bristol-WP-group.jpg 1200w" sizes="(max-width: 640px) 100vw, 640px" width="640" height="480">
+
+		</div>
 
 	</div>
 
