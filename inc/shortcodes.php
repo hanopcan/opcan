daeefd11c492052932de2884524b9624f57bdd26
Hannah Smith <hannah@opcan.co.uk>
2019-03-02 17:15:51 +0100

    Add example-work shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -18,3 +18,16 @@ function future_talks_func() {
 }
 add_shortcode( 'future-talks', 'future_talks_func' );
 
+
+/**
+ * [example-work]
+ *
+ * @package understrap
+ */
+function example_work_func() {
+
+	ob_start();
+	get_template_part( 'partials/custom-loops/projects' );
+	return ob_get_clean();
+}
+add_shortcode( 'example-work', 'example_work_func' );
