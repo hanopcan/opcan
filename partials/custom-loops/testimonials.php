ca11348ba7636e551951b9f0a4f10c69442d5d9c
hanopcan <hannah@opcan.co.uk>
2020-01-15 19:10:15 +0100

    Add title and intro text to testimonials slider

--- partials/custom-loops/testimonials.php
+++ partials/custom-loops/testimonials.php
@@ -11,6 +11,10 @@ $the_query = new WP_Query( $query_args );
 // The Loop
 if ( $the_query->have_posts() ) { ?>
 
+	<h2>Is she for real?</h2>
+
+	<p>Don’t just take my word for it! Here’s what some of my clients and previous colleagues have to say about working with me. More recommendations can be found on <a href="https://www.linkedin.com/in/hanopcan/" target="_blank" rel="noopener">LinkedIn profile</a>.</p>
+
 	<div class="flexslider">
 		<ul class="slides">
 
