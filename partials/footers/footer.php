cb27bbdd722eaa29380203ac33a18bc1956cc32f
hanopcan <hannah@opcan.co.uk>
2020-01-16 18:12:59 +0100

    Emissions check my site added to footer

--- partials/footers/footer.php
+++ partials/footers/footer.php
@@ -4,11 +4,16 @@
     </div>
 
     <div class="footer-connect">
-        <div class="footer-title">Connect with me</div>
+        <h4 class="footer__title">Connect with me</h4>
  
         <?php get_template_part('partials/social-media-icons'); ?>
 
+		<a href="https://www.websitecarbon.com/website/opcan-co-uk/" target="_blank" rel="noopener">
+			<h4 class="footer__title footer__title--small"><span class="icon fas fa-tachometer mr-2"></span>Emissions check my site</h4>
+		</a>
+
     </div>
+
 </div>
 
 
