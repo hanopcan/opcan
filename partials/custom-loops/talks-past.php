0c22ac26bdef8ed7882bdd17424b561a9317e437
hanopcan <hannah@opcan.co.uk>
2020-01-16 15:33:57 +0100

    Add pagination to past talks

--- partials/custom-loops/talks-past.php
+++ partials/custom-loops/talks-past.php
@@ -1,24 +1,29 @@
 <?php
 
+$posts_per_page = 8;
+
 $today = date('Ymd');
 	$query_args = array(
-		'post_type' 	=> 'talk',
-		'meta_query' => array(
+		'post_type' 	 => 'talk',
+		'posts_per_page' => $posts_per_page,
+		'meta_query'     => array(
 			array(
 				'key'		=> 'date',
 				'compare'	=> '<',
 				'value'		=> $today,
 			)
 			),
-		'meta_key'	=> 'date',
-		'orderby'	=> 'meta_value_num',
-		'order'		=> 'DESC'
+		'meta_key'	    => 'date',
+		'orderby'	    => 'meta_value_num',
+		'order'		    => 'DESC',
+		'paged'         => get_query_var('paged') ? get_query_var('paged') : 1
     );
     
-    $the_query = new WP_Query( $query_args );
+	$the_query = new WP_Query( $query_args );
     
 	// The Loop
 	if ( $the_query->have_posts() ) { ?>
+
 		<section class="talks-teasers-past">
     				
 			<?php 
@@ -29,6 +34,11 @@ $today = date('Ymd');
 			<?php endwhile; // End the loop ?> 
 
 		</section> 
+
+		<?php 
+		$args['total'] = (wp_count_posts( 'talk' )->publish / $posts_per_page)+1;
+		
+		understrap_pagination( $args ); ?>
          
     <?php
 	wp_reset_query(); 
