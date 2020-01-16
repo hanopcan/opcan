19dcb3b4eb8c59f825cf1458d3debbca56e6069c
hanopcan <hannah@opcan.co.uk>
2020-01-16 14:21:02 +0100

    Get rid of asset_path PHP error

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -14,8 +14,9 @@ function asset_path($filename) {
     if ( file_exists($manifest_path ) ) {
         $manifest = json_decode( file_get_contents( $manifest_path ), TRUE );
     } else {
-        $manifest = [];
-    }
+        $manifest = array();
+	}
+
     if ( array_key_exists( $filename, $manifest ) ) {
         return $manifest[$filename];
     }
@@ -29,12 +30,12 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	function understrap_scripts() {
 
 		wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:300,500&display=swap', array(), null, 'all' );
-		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/'. asset_path('css/theme.min.css'), array(), null);
+		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), null);
 
 		wp_enqueue_script( 'jquery');
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), wp_get_theme()->get( 'Version' ), true);
 
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/' . asset_path('js/theme.min.js'), array(), null, true );
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), null, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
