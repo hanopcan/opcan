f5cfae64ed9970cfcee4fc14ae422afa327b2800
hanopcan <hannah@opcan.co.uk>
2025-06-04 11:13:28 +0100

    Finish basic colour restyle

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<?php get_template_part( 'partials/banners/ds-consultant' ); ?>
+<?php get_template_part( 'partials/banners/jungle-banner' ); ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
